d8d9b1d9e8db6e72c7f29035f50e101f647367dc
Daly <dalyna64@gmail.com>
2022-10-31 23:47:46 +0100

    redux json-server

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,6 @@
-import {createStore} from "redux";
+import {applyMiddleware, createStore} from "redux";
 import reducer from "./reducer.js";
+import thunk from "redux-thunk";
 
-const store = createStore(reducer)
+const store = createStore(reducer, applyMiddleware(thunk))
 export default store
